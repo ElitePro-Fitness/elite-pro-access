13311adf78ffd640acea00537f9160bb5e2ae4eb
ElitePro-Fitness <ept.cb@icloud.com>
2026-07-02 18:26:37 -0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-document.getElementById("message").innerHTML="Le scanner sera activé à l'étape suivante.";
+document.getElementById("message").innerHTML = "Démarrage de la caméra...";
+
+const html5QrCode = new Html5Qrcode("reader");
+
+html5QrCode.start(
+    { facingMode: "environment" },
+    {
+        fps: 10,
+        qrbox: 250
+    },
+    function(decodedText) {
+
+        document.getElementById("message").innerHTML =
+            "QR détecté : " + decodedText;
+
+    },
+    function(errorMessage) {
+
+    }
+
+).catch(function(err){
+
+    document.getElementById("message").innerHTML =
+        "Erreur caméra : " + err;
+
+});
