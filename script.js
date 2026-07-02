fa6d2a5bf532e74cbec5a8c524c17eba1beb5ce1
ElitePro-Fitness <ept.cb@icloud.com>
2026-07-02 18:43:58 -0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const API =
+"https://script.google.com/macros/s/AKfycbwUc0fDv1S9YdEMmnslGakQYssQeJvgMPRSavN2VPLtr8GaM2EQ8d_hqT9RRtSNG-6c/exec";
+
 document.getElementById("message").innerHTML = "Démarrage de la caméra...";
 
 const html5QrCode = new Html5Qrcode("reader");
@@ -8,19 +11,63 @@ html5QrCode.start(
         fps: 10,
         qrbox: 250
     },
-    function(decodedText) {
+
+    async function(decodedText) {
+
+        html5QrCode.pause();
 
         document.getElementById("message").innerHTML =
-            "QR détecté : " + decodedText;
+        "Vérification...";
+
+        try {
+
+            const reponse = await fetch(API + "?id=" + encodeURIComponent(decodedText));
+
+            const client = await reponse.json();
+
+            if(client.statut=="ACTIF"){
+
+                document.body.style.background="green";
+
+                document.getElementById("message").innerHTML=
+                "✅ ACCÈS AUTORISÉ<br><br>"
+                +client.nom+" "+client.prenom;
+
+            }
+
+            else{
+
+                document.body.style.background="red";
+
+                document.getElementById("message").innerHTML=
+                "❌ ACCÈS REFUSÉ";
+
+            }
+
+        }
+
+        catch(e){
+
+            document.body.style.background="orange";
+
+            document.getElementById("message").innerHTML=
+            "Erreur : "+e;
+
+        }
+
+        setTimeout(function(){
+
+            document.body.style.background="#111";
+
+            document.getElementById("message").innerHTML=
+            "Présentez un QR Code";
+
+            html5QrCode.resume();
+
+        },3000);
 
     },
-    function(errorMessage) {
 
-    }
+    function(errorMessage){}
 
-).catch(function(err){
-
-    document.getElementById("message").innerHTML =
-        "Erreur caméra : " + err;
-
-});
+);
